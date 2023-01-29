54a70a6fc7f30ab67150f99621339cce8c2c098d
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-01-29 12:18:37 +0530

    JavaScript

--- Javascript/FullJavascript.js
+++ Javascript/FullJavascript.js
@@ -64,3 +64,51 @@ console.log(marks);
 var arr = [1, 2, "bablu", 4, 5]
 console.log(arr)
 */
+
+/*
+// Operators in JavaScript
+
+
+// Arithmetic Operators
+//var a = 100;
+//var b = 10;
+// console.log("The value of a + b is ", a+b);
+// console.log("The value of a - b is ", a-b);
+// console.log("The value of a * b is ", a*b);
+// console.log("The value of a / b is ", a/b);
+
+// Assignment Operators
+// var c = b;
+// c += 2;
+// c -= 2; // c = c - 2;
+// c *= 2;
+// c /= 2;
+// console.log(c);
+
+// Comparison Operators
+// var x = 34;
+// var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
+
+// Logical Operators
+
+// Logical and
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// Logical or
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// Logical not
+// console.log(!false);
+// console.log(!true);
+*/
