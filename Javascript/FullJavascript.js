f5f0714bd7c4c1fcb4203369f26ab38ea74529ac
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-01-31 19:01:27 +0530

    FullJavaScript

--- Javascript/FullJavascript.js
+++ Javascript/FullJavascript.js
@@ -150,3 +150,34 @@ else{
 }
 console.log("End of ladder");
 */
+
+
+/*
+// Loops in java script
+ var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         // break;
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+// arr.forEach(function ak (element){
+//     console.log(element);
+// })
+// const ac = 0;
+// ac++;
+// ac = ac +1;
+// let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j ++;
+// }
+
+// do{
+//     console.log(arr[j]);
+//     j++;
+// } while (j < arr.length);
+*/
