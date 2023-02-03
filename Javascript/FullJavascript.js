fdb823c583bc6f8e8ddd24f259b3c6620ad69e2a
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-02-03 22:44:32 +0530

    FullJavaScript

--- Javascript/FullJavascript.js
+++ Javascript/FullJavascript.js
@@ -196,3 +196,19 @@ console.log("End of ladder");
 // console.log(newLen);
 // console.log(myArr);
 */
+
+
+
+
+// String Methods in JavaScript
+/*
+let myLovelyString = "Harry is a good boy good good Harry";
+ console.log(myLovelyString.length)
+ console.log(myLovelyString.indexOf("good"))
+ console.log(myLovelyString.lastIndexOf("good"))
+
+ console.log(myLovelyString.slice(1,4))
+d = myLovelyString.replace("Harry", "Rohan");
+ d = d.replace("good", "bad");
+ console.log(d, myLovelyString)
+*/
