11c8df484042c6dda2926672cfaaa22b5f996cd3
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-02-08 23:00:18 +0530

    FulljavaScript

--- Javascript/FullJavascript.js
+++ Javascript/FullJavascript.js
@@ -254,3 +254,34 @@ tn[0].replaceChild(createdElement2, createdElement);
 
 // }
 */
+
+
+
+
+
+/*
+// Events in JavaScript
+// firstContainer.addEventListener('click', function(){
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+//     console.log("Clicked on Container")
+// })
+
+// firstContainer.addEventListener('mouseover', function(){
+//     console.log("Mouse on Container")
+// })
+
+// firstContainer.addEventListener('mouseout', function(){
+//     console.log("Mouse out of Container");
+// })
+
+// let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
+// firstContainer.addEventListener('mouseup', function(){
+//     document.querySelectorAll('.container')[1].innerHTML = prevHTML;
+//     console.log("Mouse up when clicked on Container");
+// })
+
+// firstContainer.addEventListener('mousedown', function(){
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+//     console.log("Mouse down when clicked on Container");
+// })
+*/
